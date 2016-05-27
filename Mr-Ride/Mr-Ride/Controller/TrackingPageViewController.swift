c38e3e28049cf44e88492cee97b65725ac712e3a
howard hsien <b99507020@gmail.com>
2016-05-27 16:37:38 +0800

    tracking page UI

--- Mr-Ride/Mr-Ride/Controller/TrackingPageViewController.swift
+++ Mr-Ride/Mr-Ride/Controller/TrackingPageViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class TrackingPageViewController: UIViewController {
     let classDebugInfo = "TrackingPageViewController"
     
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupMapViewStyle()
+        setupNavigationBar()
 
+    }
+    func setupNavigationBar(){
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .ShortStyle
+        self.navigationItem.title = dateFormatter.stringFromDate(NSDate())
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.whiteColor()]
+        
+    }
+    
+    func setupMapViewStyle() {
+        mapView.layer.cornerRadius = 10
     }
 
 
