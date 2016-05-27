2d6dab09ba958e1beed24c34cba3e5307a78a1ba
howard hsien <b99507020@gmail.com>
2016-05-27 16:47:19 +0800

    add few component to trackingPageViewController

--- Mr-Ride/Mr-Ride/Controller/TrackingPageViewController.swift
+++ Mr-Ride/Mr-Ride/Controller/TrackingPageViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class TrackingPageViewController: UIViewController {
     let classDebugInfo = "TrackingPageViewController"
-    
+
+    //MARK: Location and route properties
+    lazy var locationManager: CLLocationManager = {
+        var _locationManager = CLLocationManager()
+        _locationManager.delegate = self
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        _locationManager.activityType = .Fitness
+        
+        // Movement threshold for new events
+        _locationManager.distanceFilter = 10.0
+        return _locationManager
+    }()
+    lazy var locations = [CLLocation]()
+    lazy var timer = NSTimer()
+    var seconds = 0.0
+    var distance = 0.0
+
+    //MARK: UI properties
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var timeSpentLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMapViewStyle()
@@ -40,7 +60,31 @@ class TrackingPageViewController: UIViewController {
         let recordPageViewController = storyboard?.instantiateViewControllerWithIdentifier("RecordPageViewController") as! RecordPageViewController
         navigationController?.pushViewController(recordPageViewController, animated: true)
     }
-
-
-
 }
+
+// MARK: - CLLocationManagerDelegate
+extension TrackingPageViewController: CLLocationManagerDelegate {
+    func startLocationUpdates() {
+        // Here, the location manager will be lazily instantiated
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didupdate locations")
+        for location in locations {
+            if location.horizontalAccuracy < 20 {
+                //update distance
+                if self.locations.count > 0 {
+                    distance += location.distanceFromLocation(self.locations.last!)
+                }
+                
+                //save location
+                self.locations.append(location)
+            }
+        }
+        
+    }
+    
+    
+}
+
